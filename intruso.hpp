485e15983e850b54bb2a915c7879d7618b07f2bc
SkanyCst <yaggovteixeira@gmail.com>
2022-11-03 23:14:47 -0300

    update intruso.hpp

--- intruso.hpp
+++ intruso.hpp
@@ -2,16 +2,15 @@
 #include <vector>
 
 class Intruso{
-    
+
     private:
-        
-        std::string _senha;
+
         std::string _numeros;
-        std::vector<std::string> _combinacao1;
-        std::vector<std::string> _combinacao2;
-        
+        int _combinacao[5][2];
+        int _senha[6][2];
+
     public:
-        
+
         void set_senha_vazada(std::string vazou);
         std::string crack_senha();
 };
